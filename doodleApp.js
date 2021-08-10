a97a42fc0bf1a4a51543423548e00c9142825e64
Dovile Bartkeviciute <d.bartkeviciute96@gmail.com>
2021-08-10 10:15:14 +0300

    Add Platform class and random platform placement function

--- doodleApp.js
+++ doodleApp.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const doodler = document.createElement("div");
     let doodlerLeftSpace = 50;
     let doodlerBottomSpace = 150;
+    let platformCount = 5;
     let isGameOver = false;
 
     function createDoodler() {
@@ -13,9 +14,32 @@ document.addEventListener("DOMContentLoaded", () => {
         doodler.style.bottom = doodlerBottomSpace + "px";
     }
 
+    class Platform {
+        constructor(newPlatBottom) {
+            this.bottom = newPlatBottom;
+            this.left = Math.random() * 315;
+            this.visual = document.createElement("div");
+
+            const visual = this.visual;
+            visual.classList.add("platform");
+            visual.style.left = this.left + "px";
+            visual.style.bottom = this.bottom + "px";
+            grid.appendChild(visual);
+        }
+    }
+
+    function createPlatforms() {
+        for(let i = 0; i < platformCount; i++) {
+            let platGap = 600 / platformCount;
+            let newPlatBottom = 100 + i * platGap;
+            let newPlatform = new Platform(newPlatBottom);
+        }
+    }
+
     function start() {
         if (!isGameOver) {
             createDoodler();
+            createPlatforms();
         }
     }
 
